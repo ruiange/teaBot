05e72ebe11888d2aa55e77b914c2e406f9bee34d
ruiange <bombingpig@outlook.com>
2025-04-02 09:57:15 +0800

    refactor(utils): 优化 welcome函数并添加默认回复消息参数- 将 reply 变量作为函数的默认参数，提高代码可读性和可维护性 -保持 welcome_text 变量逻辑不变，确保功能一致性

--- utils/welcome.py
+++ utils/welcome.py
@@ -1,8 +1,7 @@
 from server.send_text import send_text_message
 
 
-def welcome(wcf,msg,name):
-    reply = " ⚠️新进群的朋友⚠️\n\n ⚠️请及时修改昵称（地区+摩托范昵称），以防被踢⚠️"
+def welcome(wcf,msg,name,reply = " ⚠️新进群的朋友⚠️\n\n ⚠️请及时修改昵称（地区+摩托范昵称），以防被踢⚠️"):
     welcome_text = " 欢迎新朋友【" + name + "】加入[摩托范 巧格i全国2群]🎇🎇🎇"
     send_text_message(wcf, msg.roomid, welcome_text)
     send_text_message(wcf, msg.roomid, reply)
